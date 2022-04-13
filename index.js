398081bce1ad8c154f2882822ecbc03515931792
Your Name <irving.jem@gmail.com>
2022-04-12 20:19:50 -0700

    fixing questions

--- index.js
+++ index.js
@@ -18,118 +18,115 @@ const inquirer = require('inquirer');
 // I am prompted to enter the team manager’s name, employee ID, email address, and office number
 // Manager question array
 const addManager = () => {
-    return inquirer.prompt([
-        {
-            type:'input'
-            name: 'managerName', 
-            message:'Please enter manager name:',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a valid name!');
-                    return false;
+        return inquirer.prompt([{
+                    type: 'input',
+                    name: 'managerName',
+                    message: 'Please enter manager name:',
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log('Please enter a valid name!');
+                            return false;
+                        }
+                    }
+
+                },
+                {
+                    type: 'input',
+                    name: 'managerID',
+                    message: 'Please enter manager ID:',
+                    validate: idInput => {
+                        if (isNaN(nameInput)) {
+                            console.log('Please enter a valid manager ID')
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+
+                },
+                {
+                    type: 'input',
+                    name: 'managerEmail',
+                    message: 'Please enter manager Email:'
+
+                },
+                {
+                    type: 'input',
+                    name: 'managerOfficeNumber',
+                    message: 'Please enter manager Office Number:',
+                    validate: nameInput => {
+                        if (isNaN(nameInput)) {
+                            console.log('Please enter an office number!')
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
                 }
-            } 
-            
-        },
-        {
-            type:'input'
-            name: 'managerID', 
-            message:'Please enter manager ID:', 
-            validate: idInput => {
-                if (isNaN(nameInput)) {
-                    console.log('Please enter a valid manager ID')
-                    return false;
-                } else {
-                    return true;
+            ])
+            .then(managerInput => {
+                const { managerName, managerID, managerEmail, managerOfficeNumber } = managerInput;
+                const manager = new Manager(managerName, managerID, managerEmail, managerOfficeNumber);
+
+                employees.push(manager);
+                console.log(manager);
+            })
+
+
+
+        inquirer.prompt(
+            managerQuestions
+        ).then(answer => {
+            console.log(answer);
+            //Ask for team members 
+            //convert the answers int oCLass manager object
+            const employeeOne = new Manager(answer.managerName, answer.managerID, answer.managerEmail, answer.managerOfficeNumber);
+
+            // console.log(employeeOne.id);
+            // console.log(employeeOne.getRole())
+            // console.log(employeeOne.getId()
+            //presented with a menu with the option to add an engineer or an intern or to finish building my team
+            userchoice();
+
+
+        })
+
+        function userchoice() {
+            inquirer.prompt({
+                name: 'employeeType',
+                message: 'Please choose employee type',
+                type: 'list',
+                choices: ['Engineer', 'Intern', 'None']
+            }).then(answer => {
+
+                console.log(answer);
+
+                //1. Engineer Questions 
+                if (answer.employeeType === "Engineer") {
+                    return engineerQuestions();
+                    //2. Intern Questions
                 }
-            }
-            
-        },
-        {
-            type:'input'
-            name: 'managerEmail', 
-            message:'Please enter manager Email:', 
-            
-        },
-        {
-            type:'input'
-            name: 'managerOfficeNumber', 
-            message:'Please enter manager Office Number:',
-            validate: nameInput => {
-                if  (isNaN(nameInput)) {
-                    console.log ('Please enter an office number!')
-                    return false; 
+                if (answer.employeeType === "Intern") {
+                    return internQuestions();
+                    //3. Build HTML PAGE 
                 } else {
-                    return true;
-                }
-            }
+                    fs.writeFile();
+                };
+                //helper code and template.html 
+
+            });
         }
-    ])
-    .then(managerInput => {
-        const {managerName, managerID, managerEmail, managerOfficeNumber} = managerInput;
-        const manager = new Manager (managerName, managerID, managerEmail, managerOfficeNumber);
 
-        employees.push(manager);
-        console.log(manager);
-    })
+        function engineerQuestions() {
+            inquirer.prompt({
 
+            })
+        };
 
+        function internQuestions() {
+            inquirer.prompt({
 
-inquirer.prompt(
-   managerQuestions
-).then(answer => {
-    console.log(answer);
-    //Ask for team members 
-    //convert the answers int oCLass manager object
-    const employeeOne = new Manager(answer.managerName, answer.managerID, answer.managerEmail, answer.managerOfficeNumber);
-
-    // console.log(employeeOne.id);
-    // console.log(employeeOne.getRole())
-    // console.log(employeeOne.getId()
-    //presented with a menu with the option to add an engineer or an intern or to finish building my team
-    userchoice(); 
-
-
-})
-
-function userchoice(){
-    inquirer.prompt(
-        {
-            name: 'employeeType',
-            message: 'Please choose employee type'
-            type: 'list',
-            choices: ['Engineer', 'Intern', 'None']
-        }
-     ).then(answer => {
-     
-         console.log(answer);
-
-        //1. Engineer Questions 
-         if(answer.employeeType === "Engineer"){
-            return engineerQuestions();
-        //2. Intern Questions
-         } if (answer.employeeType === "Intern") {
-             return internQuestions();
-        //3. Build HTML PAGE 
-         }else {
-        fs.writeFile();
-    };
-    //helper code and template.html 
-
-     });
-}
-
-function engineerQuestions(){
-    inquirer.prompt({
-        
-    })
-};
-
-function internQuestions(){
-    inquirer.prompt({
-
-    })
-};
-
+            })
+        };
