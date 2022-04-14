4fb72f03c341fd168c29d7f6aeba75b3dabb9d74
Your Name <irving.jem@gmail.com>
2022-04-14 10:11:45 -0700

    added better sytles to css file

--- src/template.js
+++ src/template.js
@@ -90,14 +90,14 @@ module.exports = employees => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>My Team</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link href="style.css" rel="stylesheet">
     </head>
     <body>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 jumbotron mb-3" id="title">
-                    <h1 class="text-center">My Team</h1>
+                    <h1 class="text-center">The Dream Team</h1>
                 </div>
             </div>
         </div>
