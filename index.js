cf3c50ade564c3ae0d6e07795cd88deeb710d91a
Your Name <irving.jem@gmail.com>
2022-04-14 09:33:16 -0700

    trying switch functionality in js sending commit incase things break

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-console.log("welcome to team profile generator....");
-
 // creating page
-const templateHTML = require('./src/template.HTML')
+const templateHTML = require('./src/template.js')
 
 // team profiles
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
 
 // employee array
-const employees = [];
+const employeesArray = [];
 
 // node modules
 const fs = require('fs')
@@ -18,7 +17,9 @@ const inquirer = require('inquirer');
 // I am prompted to enter the team manager’s name, employee ID, email address, and office number
 // Manager question array
 const addManager = () => {
-    return inquirer.prompt([{
+    console.log('Time to build your teams profile.')
+    return inquirer
+        .prompt([{
                 type: 'input',
                 name: 'managerName',
                 message: 'Please enter manager name:',
@@ -35,97 +36,124 @@ const addManager = () => {
             {
                 type: 'input',
                 name: 'managerID',
-                message: 'Please enter manager ID:'
+                message: 'Please enter manager ID:',
+                validate: idInput => {
+                    if (idInput === '') {
+                        console.log("Please enter the manager's ID number!");
+                        return false;
+                    }
+                    return true;
+                }
 
             },
             {
                 type: 'input',
                 name: 'managerEmail',
-                message: 'Please enter manager Email:'
+                message: 'Please enter manager Email:',
+                validate: emailInput => {
+                    if (emailInput === '') {
+                        console.log("Please enter the manager's email address!");
+                        return false;
+                    }
+                    return true;
+                }
 
             },
             {
                 type: 'input',
                 name: 'managerOfficeNumber',
-                message: 'Please enter manager Office Number:'
+                message: 'Please enter manager Office Number:',
+                validate: officeNumberInput => {
+                    if (officeNumberInput === '') {
+                        console.log('Please enter the manager office number!')
+                        return false;
+                    }
+                    return true;
+                }
             }
         ])
-        .then(managerInput => {
-            const { managerName, managerID, managerEmail, managerOfficeNumber } = managerInput;
-            const manager = new Manager(managerName, managerID, managerEmail, managerOfficeNumber);
-
-            employees.push(manager);
+        // managed created from answers
+        .then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
+            // push to array
+            employeesArray.push(manager);
             console.log(manager);
+
+            addEmployee();
         })
 };
-
+// adding employee
 const addEmployee = () => {
-    console.log(`
-    =================
-    Adding employees to the team
-    =================
-    `);
 
-    return inquirer.prompt([{
-                type: 'list',
-                name: 'role',
-                message: "Please choose your employee role",
-                choices: ['Engineer', 'Intern']
-            },
-            {
-                type: 'input',
-                name: 'name',
-                message: "Name of the Employee?",
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: 'Please enter a valid employee ID'
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: "Please enter employee email"
-            },
-            {
-                type: 'input',
-                name: 'github',
-                message: "Please enter the employee's github username."
-            },
-            {
-                type: 'input',
-                name: 'school',
-                message: "Please enter the intern's school"
-            },
-            {
-                type: 'confirm',
-                name: 'confirmAddEmployee',
-                message: "Would you like to add another team member?",
-                default: false
-            }
-        ])
-        .then(employeeData => {
-            let { name, id, email, role, school, confirmAddEmployee } = employeeData
-            let employee;
+return inquirer
+    .prompt([{
+        type: 'list',
+        name: 'role',
+        message: "Please choose your employee role",
+        choices: ['Engineer', 'Intern', 'None']
+    }])
+    .then((choice.userChoice) {
+        const expr = 'Papayas';
+        switch (expr) {
+            case 'Oranges':
+                console.log('Oranges are $0.59 a pound.');
+                break;
+            case 'Mangoes':
+            case 'Papayas':
+                console.log('Mangoes and papayas are $2.79 a pound.');
+                // expected output: "Mangoes and papayas are $2.79 a pound."
+                break;
+            default:
+        }
+    }); {
+    type: 'input',
+    name: 'name',
+    message: "Name of the Employee?",
+}, {
+    type: 'input',
+    name: 'id',
+    message: 'Please enter a valid employee ID'
+}, {
+    type: 'input',
+    name: 'email',
+    message: "Please enter employee email"
+}, {
+    type: 'input',
+    name: 'github',
+    message: "Please enter the employee's github username."
+}, {
+    type: 'input',
+    name: 'school',
+    message: "Please enter the intern's school"
+}, {
+    type: 'confirm',
+    name: 'confirmAddEmployee',
+    message: "Would you like to add another team member?",
+    default: false
+}
+])
+.then(employeeData => {
+    let { name, id, email, role, school, confirmAddEmployee } = employeeData
+    let employee;
 
-            if (role === "Engineer") {
-                employee = new Engineer(name, id, email, github)
+    if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github)
 
-                console.log(employee)
-            } else if (role === "intern") {
-                employee = new Intern(name, id, email, school);
+        console.log(employee)
+    } else if (role === "intern") {
+        employee = new Intern(name, id, email, school);
 
-                console.log(employee)
-            }
+        console.log(employee)
+    }
 
-            employees.push(employees)
+    employees.push(employees)
 
-            if (confirmAddEmployee) {
-                return addEmployee(employees);
-            } else {
-                return employees;
-            }
-        })
+    if (confirmAddEmployee) {
+        return addEmployee(employees);
+    } else {
+        return employees;
+    }
+})
 };
 
 
---
+++ src/template.js
@@ -0,0 +1,116 @@
+// generates html page
+const generatePage = team => {
+    // Manager card
+    const managerCard = manager => {
+        return `
+        <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
+
+    // create engineer card
+    const engineerCard = engineer => {
+        return `
+        <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i> ${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
+
+    // create intern card
+    const internCard = intern => {
+        return `
+        <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i> ${intern.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item">School: ${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
+
+    // array cards
+    const html = [];
+
+    // push employee cards to html array
+    html.push(team
+        // filters through team array for manager(s)
+        .filter(employee => employee.getRole() === "Manager")
+        // calls managerCard() function on each manager
+        .map(manager => managerCard(manager)));
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => engineerCard(engineer))
+        // joins all the elements in the array and returns it as a string
+        .join(""));
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => internCard(intern))
+        .join(""));
+
+    // returns html array as a joined string
+    return html.join("");
+};
+
+// export function to generate html page
+module.exports = team => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3" id="title">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    ${generatePage(team)}
+                </div>
+            </div>
+        </div>
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+    </body>
+    </html> 
+    `;
+};
